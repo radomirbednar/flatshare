855a269fcd17ac904fec0fd5e55b992224c23ab6
ales <ales@ales-PC>
2015-11-12 15:01:11 +0100

    .

--- wp-content/themes/wpresidence/templates/video_base.php
+++ wp-content/themes/wpresidence/templates/video_base.php
@@ -3,11 +3,11 @@
         <div class="video-container">
             <div class="filter"></div>
             <video autoplay loop class="fillWidth">
-                <source src="<?php echo get_template_directory_uri() ?>/video/drums/MP4/Drums.mp4" type="video/mp4" />Your browser does not support the video tag. I suggest you upgrade your browser.
-                <source src="<?php echo get_template_directory_uri() ?>/video/drums/WEBM/Drums.webm" type="video/webm" />Your browser does not support the video tag. I suggest you upgrade your browser.
+                <source src="<?php echo get_template_directory_uri() ?>/video/meetingroom/MP4/Meeting-Room.mp4" type="video/mp4" />Your browser does not support the video tag. I suggest you upgrade your browser.
+                <source src="<?php echo get_template_directory_uri() ?>/video/meetingroom/WEBM/Meeting-Room.webm" type="video/webm" />Your browser does not support the video tag. I suggest you upgrade your browser.
             </video>
             <div class="poster hidden">
-                <img src="<?php echo get_template_directory_uri() ?>/video/drums/Snapshots/Drums.jpg" alt="">
+                <img src="<?php echo get_template_directory_uri() ?>/video/meetingroom/Snapshots/Meeting-Room.jpg" alt="">
             </div>
         </div>
     </div>
