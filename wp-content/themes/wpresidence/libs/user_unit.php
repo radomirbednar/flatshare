92c21f98603a2f9dfab8b07e8c5a0e5315d785c4
brunofritsch <brunofritsch@10.0.0.5>
2015-12-04 10:56:44 +0100

    first sentence dot ending - not ideal

--- wp-content/themes/wpresidence/libs/user_unit.php
+++ wp-content/themes/wpresidence/libs/user_unit.php
@@ -9,26 +9,23 @@
 <div class="col-md-3 listing_wrapper">
     <div class="agent_unit" data-link="<?php print $author_url; ?>"> 
         <div class="agent-unit-img-wrapper person-<?php echo (int) $q->ID ?>">
-            <?php
-             
+            <?php 
             print $thumb_prop;
             print '<div class="listing-cover"></div>
                    <a href="' . $author_url . '"> <span class="listing-cover-plus">+</span></a>';
             ?>   
             <span class="user_euro_unit">  
-                <?php
-               
+                <?php 
                 if ($rent_amount != '') {
                     print __('', 'wpestate') . ' ' . wpestate_show_price_floor($rent_amount, $currency, $where_currency, 1);
-                }
-                
+                } 
                 ?>  
             </span>   
         </div> 
         <div class="user_unit_info">
-            <?php
+            <?php 
             print '<h4> <a href="' . $author_url . '">' . esc_attr($first_name) . ' ' . esc_attr($last_name) . '</a></h4> 
-                   <div class="agent_position"> ' .__('Looking for ', 'wpestate') .esc_attr( $looking_for_array[$looking_for][1]) .' in '. esc_attr( $looking_where ) . '</div>';   
+                   <div class="agent_position"> ' .__('Looking for ', 'wpestate') .esc_attr( isset($looking_for_array[$looking_for][1])) .' in '. esc_attr( $looking_where ) . '</div>';   
             if ($user_age) {
                 print '<div class="agent_detail">' . __('Age', 'wpestate') . ': ' . esc_attr($user_age) . '</div>';
             }
