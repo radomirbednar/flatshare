a7c1106691f54a4b0feb767a218de9cef3f6692e
brunofritsch <brunofritsch@10.0.0.5>
2015-12-08 13:50:21 +0100

    Merge origin/Radek-dev into Radek-dev
    Conflicts:
    	UniAdapters/WPEcommerce/BinderTemplate/[@UniModul]Binder/wp-content/plugins/wp-e-commerce/wpsc-merchants/[@UniModul]Binder[@SubMethod].merchant.php

--- wp-content/themes/wpresidence/templates/half_map_core.php
+++ wp-content/themes/wpresidence/templates/half_map_core.php
@@ -64,13 +64,15 @@ get_template_part('templates/property_ajax_tax_hidden_filters');
    
                 if ($show_save_search=='yes' ){
                     if( is_user_logged_in() ){
-                        print '<div class="search_unit_wrapper advanced_search_notice">';
+                        print '<div class="search_unit_wrapper advanced_search_notice">'; 
+                        
+                        /*
                         print '<div class="search_param"><strong>'.__('Search Parameters: ','wpestate').'</strong>';
                             wpestate_show_search_params($args,$custom_advanced_search, $adv_search_what,$adv_search_how,$adv_search_label);
                         print'</div>';
-                        print'</div>';
-
-
+                        */
+                         
+                        print'</div>'; 
                         print '<div class="saved_search_wrapper"> <span id="save_search_notice">'.__('Save this Search?','wpestate').'</span>'; 
                         print '<input type="text" id="search_name" class="new_search_name" placeholder="'.__('Search name','wpestate').'">';
                         print '<button class="wpb_button  wpb_btn-info wpb_btn-large" id="save_search_button">'.__('Save Search','wpestate').'</button>';
